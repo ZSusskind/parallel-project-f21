d93bea86774f660e62c08ae76bf6f9af57117a93
Zachary Susskind <ZSusskind@utexas.edu>
2021-11-07 15:40:06 -0600

    Add sweeping script; update make_scene to generate the occluding body deterministically

--- make_scene.py
+++ make_scene.py
@@ -2,9 +2,12 @@
 
 import sys
 import math
+import argparse
 import numpy as np
-from random import random
+from random import random, seed
+#from numba import jit, prange # unfortunately, Numba doesn't seem to play nicely with RNG seeds
 
+#@jit(nopython=True)
 def make_triangle(target_area, is_foreground):
     min_angle = math.radians(30)
     max_angle = math.radians(150)
@@ -21,12 +24,12 @@ def make_triangle(target_area, is_foreground):
         [base_length,                               0.0,                                    0.0],
         [base_length*leg_ratio*math.cos(angle),     base_length*leg_ratio*math.sin(angle),  0.0]
     ])
-    assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
+    #assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
     
     # Translate the triangle so it is centered at (0, 0, 0)
     centroid = (1/3) * coordinates.sum(axis=0)
     coordinates -= centroid
-    assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
+    #assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
 
     # Randomly yaw, pitch, and roll the triange
     yaw = random() * math.tau
@@ -52,8 +55,9 @@ def make_triangle(target_area, is_foreground):
             math.cos(pitch)*math.cos(roll)
         ]
     ])
-    coordinates = np.asarray([np.dot(rotation_matrix, c) for c in coordinates])
-    assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
+    coordinates = np.dot(rotation_matrix, coordinates.T).T
+    #np.einsum("mn,bn->bm", rotation_matrix, coordinates)
+    #assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
 
     # Move the triangle to a random position in the scene
     min_translate = 0.1
@@ -65,22 +69,45 @@ def make_triangle(target_area, is_foreground):
     else:
         z_translate = -100
     coordinates += np.array([x_translate, y_translate, z_translate])
-    assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
+    #assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
 
     # Ensure the coordinates are in "clockwise" order to fix the orientation of the normal
     normal = np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0])
     if normal[2] > 0:
-        coordinates = np.asarray([coordinates[0], coordinates[2], coordinates[1]])
-    assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
-    assert(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0])[2] <= 0)
+        coordinates = np.dot(np.array([[1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0]]), coordinates)
+        #coordinates[[0, 2, 1]] #np.asarray([coordinates[0], coordinates[2], coordinates[1]])
+    #assert(math.isclose(np.linalg.norm(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0]))/2, target_area))
+    #assert(np.cross(coordinates[1]-coordinates[0], coordinates[2]-coordinates[0])[2] <= 0)
 
     return coordinates
 
-num_triangles = int(sys.argv[1])
-out_fname = sys.argv[2]
+#@jit(nopython=True)
+def make_triangles(num_triangles, average_size, size_range):
+    min_size = average_size - (size_range / 2)
+    triangles = np.empty((num_triangles, 3, 3), dtype=np.float64)
+    for i in range(num_triangles):
+        triangles[i] = make_triangle(size_range*random()+min_size, False)
+    return triangles
 
-triangles = [make_triangle((0.06*random())+0.02, False).flatten().tolist() for i in range(num_triangles)]
-triangles.append(make_triangle(0.5, True).flatten().tolist()) # Occluding body
-out_str = "\n".join(",".join(str(v) for v in t) for t in triangles)
-with open(out_fname, "w") as f:
-    f.write(out_str)
+def make_scene(background_triangles, out_fname, rand_seed=None, average_size=0.05, size_range=0.06):
+    if rand_seed is not None:
+        seed(rand_seed)
+
+    triangles = make_triangles(background_triangles, average_size, size_range)
+    triangle_list = [t.flatten().tolist() for t in triangles.reshape(-1, 9)]
+    triangle_list.append([0.15, 0.15, -100, 0.5, 0.85, -100, 0.85, 0.15, -100]) # Occluding body
+    out_str = "\n".join(",".join(str(v) for v in t) for t in triangle_list)
+    with open(out_fname, "w") as f:
+        f.write(out_str)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("background_triangles", type=int, help="Number of triangles to generate in the image background")
+    parser.add_argument("--out_fname", "-o", required=True, help="Output filename (standard extension is .tri)")
+    parser.add_argument("--seed", type=int, help="Specify random seed")
+    parser.add_argument("--average_size", type=float, default=0.05, help="Average size of triangles")
+    parser.add_argument("--size_range", type=float, default=0.06, help="Difference in size between smallest and largest random triangles; uniform distribution")
+    args = parser.parse_args()
+
+    make_scene(args.background_triangles, args.out_fname, args.seed, args.average_size, args.size_range)
+
